a08ef86e93f3b50b1c3b7d98456260d8bc85326f
Mike Solomon <msolomon@thrillist.com>
2015-10-14 22:45:41 -0400

    test route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
-  root 'calendar#redirect'
+  root 'welcome#index'
+  #get 'auth' => 'calendar#redirect'
+  get 'auth' => 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
